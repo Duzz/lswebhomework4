ed2d8d7b14303593ff6aadc8e84bb895797fb47f
Duzz <Looper@timothys-mini.attlocal.net>
2017-05-26 21:35:26 -0500

    Updated

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 //Change the background color of the entire document to red
-document.body.style.backgroundColor = "red";
+document.body.style.backgroundColor = 'red';
 
-//Change the background color of just the "about me" portion to blue
-//document.getElementsByTagName("h1").style.backgroundColor = "blue";
-document.querySelector("h1").style.backgroundColor = "blue";
+//Change the background color of just the 'about me' portion to blue
+//document.getElementsByTagName('h1').style.backgroundColor = 'blue';
+document.querySelector('h1').style.backgroundColor = 'blue';
 
 //Change the font-style of the entire document to sans-serif
-
+document.body.style.fontFamily = 'sans-serif';
 //Add your nickname
-document.getElementById("nickname").innerHTML = "Looper";
+document.getElementById('nickname').innerHTML = 'Looper';
 
 //Add your favorites
-document.getElementById("favorites").innerHTML = "Linux";
+document.getElementById('favorites').innerHTML = 'Linux';
 
 //Add your hometown
-document.getElementById("hometown").innerHTML = "irc";
+document.getElementById('hometown').innerHTML = 'irc';
 
 //## Extra Credit (still only using JavaScript) ##
 
-//Iterate through each li and change the class to "listitem". Add a style tag that sets a rule for "listitem" to make the color red.
+//Iterate through each li and change the class to 'listitem'. Add a style tag that sets a rule for 'listitem' to make the color red.
 var lis = document.getElementByTagName('li');
 for (var i = 0; i < lis.length; i++) {
     lis[i].className += ' listitem';
